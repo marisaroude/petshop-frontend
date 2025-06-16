2924f23bfc8074a1845e5ec7239c32c164b83e10
natasha grasso <natiimetralle27@gmail.com>
2025-06-16 12:30:17 -0300

    Preguntas-Pendientes

--- src/components/menu/AdminMenu.js
+++ src/components/menu/AdminMenu.js
@@ -70,7 +70,9 @@ export default function AdminMenu() {
           </button>
         </MenuItem>
         <MenuItem>
-          <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+          <button
+            onClick={() => router.push('/admin/preguntas-pendientes')}
+            className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
             <QuestionMarkCircleIcon className="size-4 fill-black" />
             Preguntas Pendientes
           </button>

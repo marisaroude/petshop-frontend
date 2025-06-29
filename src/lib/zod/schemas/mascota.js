4de4231c0d11db379de8cf5aa0ebacee9edbb903
Marisa Roude <marisa.roude@devlabs.dev>
2025-06-29 17:34:22 -0300

    description rquired

--- src/lib/zod/schemas/mascota.js
+++ src/lib/zod/schemas/mascota.js
@@ -4,7 +4,7 @@ import { z } from 'zod'
 export const mascotaSchema = z.object({
   name: z.string().min(1, 'El nombre de la mascota es obligatorio'),
   type: z.string().min(1, 'El tipo de la mascota es obligatoria'),
-  description: z.string(),
+  description: z.string().min(1, 'La descripcion de la mascota es obligatoria'),
   race: z.string(),
 })
 
